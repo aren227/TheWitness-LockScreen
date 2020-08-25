381cd0f5ef3879dd06c8b57b1b12e64bafc1830c
aren227 <mathdong01@gmail.com>
2020-08-25 14:32:39 +0900

    Implement more interesting maze puzzle

--- app/src/main/java/com/aren/thewitnesspuzzle/puzzle/walker/RandomGridTreeWalker.java
+++ app/src/main/java/com/aren/thewitnesspuzzle/puzzle/walker/RandomGridTreeWalker.java
@@ -2,7 +2,14 @@ package com.aren.thewitnesspuzzle.puzzle.walker;
 
 import android.util.Log;
 
+import com.aren.thewitnesspuzzle.math.Vector2Int;
+import com.aren.thewitnesspuzzle.math.Vector3Int;
+import com.aren.thewitnesspuzzle.math.Vector4Int;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 
 public class RandomGridTreeWalker {
 
@@ -15,6 +22,7 @@ public class RandomGridTreeWalker {
 
     public int[][] travelOrder = {{0,1,2,3}, {0,1,3,2}, {0,2,1,3}, {0,2,3,1}, {0,3,1,2}, {0,3,2,1}, {1,0,2,3}, {1,0,3,2}, {1,2,0,3}, {1,2,3,0}, {1,3,0,2}, {1,3,2,0}, {2,0,1,3}, {2,0,3,1}, {2,1,0,3}, {2,1,3,0}, {2,3,0,1}, {2,3,1,0}, {3,0,1,2}, {3,0,2,1}, {3,1,0,2}, {3,1,2,0}, {3,2,0,1}, {3,2,1,0}};
     public int[][] delta = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    public int[] opposite = {1, 0, 3, 2};
 
     public void walk(int x, int y, int depth){
         visited[x][y] = true;
@@ -32,7 +40,7 @@ public class RandomGridTreeWalker {
         }
     }
 
-    public RandomGridTreeWalker(int width, int height, Random random, int startX, int startY){
+    public RandomGridTreeWalker(int width, int height, Random random, int startX, int startY, float newBranchProb){
         this.width = width;
         this.height = height;
         this.random = random;
@@ -43,7 +51,54 @@ public class RandomGridTreeWalker {
         dist = new int[width + 1][height + 1];
         direction = new int[width + 1][height + 1];
 
-        walk(startX, startY, 0);
+        List<Stack<Vector4Int>> stacks = new ArrayList<>(); // (x, y, dist, direction)
+        stacks.add(new Stack<Vector4Int>());
+        stacks.get(0).add(new Vector4Int(startX, startY, 0, 0));
+
+        while(true){
+            int check = 0;
+            for(Stack<Vector4Int> stack : stacks) check += stack.size();
+            if(check == 0) break;
+
+            int idx = 0;
+            while(idx < stacks.size()){
+                Stack<Vector4Int> stack = stacks.get(idx++);
+
+                if(stack.isEmpty()) continue;
+                Vector4Int v = stack.peek();
+
+                if(v.x < 0 || v.x > width || v.y < 0 || v.y > height || visited[v.x][v.y]){
+                    stack.pop();
+                    continue;
+                }
+
+                visited[v.x][v.y] = true;
+                dist[v.x][v.y] = v.z;
+                if(!(v.x == startX && v.y == startY)){
+                    direction[v.x + delta[v.w][0]][v.y + delta[v.w][1]] |= 1 << opposite[v.w];
+                }
+
+                int order = random.nextInt(24);
+                for(int i = 0; i < 4; i++){
+                    int nx = v.x + delta[travelOrder[order][i]][0];
+                    int ny = v.y + delta[travelOrder[order][i]][1];
+
+                    // New branch
+                    if(random.nextFloat() < newBranchProb){
+                        Stack<Vector4Int> newStack = new Stack<>();
+                        newStack.add(new Vector4Int(nx, ny, v.z + 1, opposite[travelOrder[order][i]]));
+                        stacks.add(newStack);
+                    }
+                    else{
+                        stack.add(new Vector4Int(nx, ny, v.z + 1, opposite[travelOrder[order][i]]));
+                    }
+                }
+            }
+        }
+
+        //walk(startX, startY, 0);
     }
+
+
 
 }
