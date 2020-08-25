819e698a855ef7767e96d9e06deb7369f747bc6e
aren227 <mathdong01@gmail.com>
2020-08-25 18:36:42 +0900

    Adjust some background colors, Close #5

--- app/src/main/java/com/aren/thewitnesspuzzle/puzzle/color/PalettePreset.java
+++ app/src/main/java/com/aren/thewitnesspuzzle/puzzle/color/PalettePreset.java
@@ -13,18 +13,18 @@ public class PalettePreset {
         register("General_Slide", "#fd9700", "#8a4700", "#fffef9", "#fbe50d", "#050a0f", 0.5f);
         register("General_Panel", "#babcb9", "#5a4d3f", "#fccf97", "#ff8326", "#050a0f", 0.3f);
         register("Entry_1", "#ffcb00", "#422c06", "#feffc5", "#fef656", "#050a0f", 1.0f);
-        register("Green_1", "#00ee4a", "#006615", "#eaeabc", "#b8ff14", "#050a0f", 0.5f);
-        register("Blue_1", "#4f4cff", "#1405a1", "#aaccf4", "#659cff", "#050a0f", 0.5f);
+        register("Green_1", "#0ee851", "#006615", "#eaeabc", "#b8ff14", "#050a0f", 0.5f);
+        register("Blue_1", "#514eed", "#1405a1", "#aaccf4", "#659cff", "#050a0f", 0.5f);
         register("GlassFactory_1", "#96bce5", "#374b8d", "#e4e6e1", "#18fe9d", "#050a0f", 0.2f);
         register("SymmetryIsland_1", "#96c2e9", "#555553", "#fafafa", "#fafafa", "#050a0f", 0.2f); // cursor color is cyan & yellow
-        register("Swamp_1", "#b7c900", "#e9e370", "#f5f6ab", "#fcf31a", "#050a0f", 0.0f);
-        register("Swamp_2", "#e09b19", "#dbbc6b", "#f4e791", "#f2f528", "#050a0f", 0.0f);
-        register("Swamp_3", "#be0802", "#ef4632", "#f1f2d2", "#fdf61a", "#050a0f", 0.0f);
+        register("Swamp_1", "#9fad17", "#dbd670", "#f5f6ab", "#fcf31a", "#050a0f", 0.0f);
+        register("Swamp_2", "#c28b25", "#e6c97e", "#f4e791", "#f2f528", "#050a0f", 0.0f);
+        register("Swamp_3", "#992c29", "#e35746", "#f1f2d2", "#fdf61a", "#050a0f", 0.0f);
         register("Treehouse_1", "#505b5f", "#0f1412", "#fef7e1", "#fda20f", "#050a0f", 0.0f);
         register("Treehouse_2", "#505b5f", "#0f1412", "#fef7e1", "#ac01be", "#050a0f", 0.0f);
         register("Treehouse_3", "#505b5f", "#0f1412", "#fef7e1", "#00fa00", "#050a0f", 0.0f);
         register("Quarry_1", "#363838", "#59877c", "#ffffff", "#ffffff", "#050a0f", 1.0f);
-        register("Challenge_1", "#38d185", "#535c53", "#fad8ca", "#ffd49c", "#050a0f", 0.2f);
+        register("Challenge_1", "#40c985", "#535c53", "#fad8ca", "#ffd49c", "#050a0f", 0.2f);
         register("Challenge_2", "#000000", "#79600e", "#ffd585", "#fe9f00", "#050a0f", 1.0f);
     }
 
